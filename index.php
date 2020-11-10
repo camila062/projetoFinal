3fd88fd000ddb2b1bd42560f82ea56276e7124e5
camila062 <camilalidiaaraujo@gmail.com>
2020-11-10 14:08:45 -0300

    alteração

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="">
+
+    <!-- outros -->
+    <link rel="stylesheet" href="">
+
+    <!-- meu css -->
+    <link rel="stylesheet" href="">
+
     <title>Projeto final</title>
 </head>
+
 <body>
-    
+
+    <!-- Header -->
+    <!-- Fim Header -->
+
+    <!-- Páginas do meu site -->
+    <!-- Fim Páginas do meu site -->
+
+    <!-- Footer  -->
+
 </body>
+
 </html>
