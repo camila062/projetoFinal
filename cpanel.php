b5648d75a3713dc7e905bff7f2a698b3548f8310
camila062 <camilalidiaaraujo@gmail.com>
2020-11-17 16:11:53 -0300

    alteração6

--- cpanel.php
+++ cpanel.php
@@ -17,21 +17,45 @@ if (isset($_SESSION['usuario'])) {
 
     switch ($pg) {
         case 'cpanel':
-            include_once "app/painelAdm/index.php";
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/inicial.php";
+            include_once "app/painelAdm/paginas/includes/footer.php";
+            break;
+
+        case 'contato':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/contato.php";
+            include_once "app/painelAdm/paginas/includes/footer.php";
+            break;
+
+        case 'produtos':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/produtos.php";
+            include_once "app/painelAdm/paginas/includes/footer.php";
             break;
 
         case 'sair':
-
+            session_destroy();
+            Header('Location:' . $_SERVER['PHP_SELF']);
             break;
 
         default:
-            include_once "app/painelAdm/index.php";
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/inicial.php";
+            include_once "app/painelAdm/paginas/includes/footer.php";
             break;
     }
 } else {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (verificaSeLogado()) {
-            include_once "app/painelAdm/index.php";
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/inicial.php";
+            include_once "app/painelAdm/paginas/includes/footer.php";
         }
     } else {
         include_once "app/painelAdm/paginas/login.php";
